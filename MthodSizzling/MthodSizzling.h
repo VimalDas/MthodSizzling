ea1e53cf1ce358cb0bce05f91858dfe7992ec6b0
Vimal Das <vimaldasu@gmail.com>
2023-11-11 02:55:35 +0530

    Initial Commit

---
+++ MthodSizzling/MthodSizzling.h
@@ -0,0 +1,18 @@
+//
+//  MthodSizzling.h
+//  MthodSizzling
+//
+//  Created by Vimal Das on 11/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MthodSizzling.
+FOUNDATION_EXPORT double MthodSizzlingVersionNumber;
+
+//! Project version string for MthodSizzling.
+FOUNDATION_EXPORT const unsigned char MthodSizzlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MthodSizzling/PublicHeader.h>
+
+
